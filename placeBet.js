55c0602a50838d766467026a7fb906e972c904b9
Cmendence <chris.mendence@gmail.com>
2023-04-18 14:38:03 -0400

    major update

--- placeBet.js
+++ placeBet.js
@@ -3,8 +3,9 @@
 
 function placeBetFourPayoff(bet) {
     let payoff = ((bet / 5) * 9);
+    document.getElementById('payoff-four').textContent=`Bet! $${bet} pays $${payoff}`;
+
     console.log(`paying bet: $${bet} on the 4 amount: $${payoff}`);
-    return payoff;
 }
 
 function isFourBetValid(bet) {
@@ -13,46 +14,43 @@ function isFourBetValid(bet) {
         document.getElementById('four-invalid').textContent='';
         return true;
     } else {
-        document.getElementById('four-invalid').textContent= closestProperFourBet(bet);
+        document.getElementById('place-four').classList.add('is-invalid');
+        document.getElementById('four-invalid').textContent= closestBetOutsideNums(bet);
         return false;
     }
 }
 
 
-function closestProperFourBet(bet) {
-    let closestBet = Math.round(bet / 5)* 5;
-    return `${bet} is not proper for the 4. Nearest proper bet is ${closestBet}`;
-}
+
 
 
 //FIVE
 
 function placeBetFivePayoff(bet) {
     let payoff = ((bet / 5) * 7);
+    document.getElementById('payoff-five').textContent=`Bet! $${bet} pays $${payoff}`;
     console.log(`paying bet: $${bet} on the 5 amount: $${payoff}`);
-    return payoff;
 }
 
 function isFiveBetValid(bet) {
     console.log(`isFiveBetValid checked for proper bet: ${bet} on the 5`);
     if (bet >= 5, bet % 5 === 0) {
+        document.getElementById('five-invalid').textContent='';
         return true;
     } else {
+        document.getElementById('place-five').classList.add('is-invalid');
+        document.getElementById('five-invalid').textContent= closestBetOutsideNums(bet);
         return false;
     }
-}
-
-function closestProperFiveBet(bet) {
-    let closestBet = Math.round(bet / 5)* 5;
-    return `${bet} is not proper for the 5. Nearest proper bet is ${closestBet}`;
 }
 
 //SIX
 
 function placeBetSixPayoff(bet) {
     let payoff = ((bet / 6) * 7);
+    document.getElementById('payoff-six').textContent=`Bet! $${bet} pays $${payoff}`;
+
     console.log(`paying bet: $${bet} on the 6 amount: $${payoff}`);
-    return payoff;
 }
 
 
@@ -63,22 +61,19 @@ function isSixBetValid(bet) {
 
         return true;
     } else {
-        document.getElementById('six-invalid').textContent= closestProperSixBet(bet);
+        document.getElementById('place-six').classList.add('is-invalid');
+        document.getElementById('six-invalid').textContent= closestSixEightBet(bet);
         return false;
     }
-    }
-
-function closestProperSixBet(bet) {
-    let closestBet = Math.round(bet / 6)*6;
-        return `${bet} is not proper for the 6. Nearest proper bet is ${closestBet}`;
     }
 
 //EIGHT
 
 function placeBetEightPayoff(bet) {
     let payoff = ((bet / 6) * 7);
+    document.getElementById('payoff-eight').textContent=`Bet! $${bet} pays $${payoff}`;
     console.log(`paying bet: $${bet} on the 8 amount: $${payoff}`);
-    return payoff;
+
 }
 
 function isEightBetValid(bet) {
@@ -87,14 +82,15 @@ function isEightBetValid(bet) {
         document.getElementById('eight-invalid').textContent='';
         return true;
     } else {
-        document.getElementById('eight-invalid').textContent= closestProperEightBet(bet);
+        document.getElementById('place-eight').classList.add('is-invalid');
+        document.getElementById('eight-invalid').textContent= closestSixEightBet(bet);
         return false;
     }
     }
 
 function closestProperEightBet(bet) {
         let closestBet = Math.round(bet / 6)*6;
-        return `${bet} is not proper for the 8. Nearest proper bet is ${closestBet}`;
+        return `${bet} is not a proper bet. Nearest proper bet is ${closestBet}`;
     }
 
 
@@ -102,28 +98,30 @@ function closestProperEightBet(bet) {
 
 function placeBetNinePayoff(bet) {
     let payoff = ((bet / 5) * 7);
+    document.getElementById('payoff-nine').textContent=`Bet! $${bet} pays $${payoff}`;
+
     console.log(`paying bet: $${bet} on the 9 amount: $${payoff}`);
-    return payoff;
 }
 
 function isNineBetValid(bet) {
     console.log(`isNineBetValid checked for proper bet: ${bet} on the 9`);
     if (bet >= 5, bet % 5 === 0) {
+        document.getElementById('nine-invalid').textContent='';
+
         return true;
     } else {
+        document.getElementById('place-nine').classList.add('is-invalid');
+        document.getElementById('nine-invalid').textContent= closestBetOutsideNums(bet);
         return false;
     }
-}
-
-function closestProperNineBet(bet) {
-    let closestBet = Math.round(bet / 5)* 5;
-    return `${bet} is not proper for the 9. Nearest proper bet is ${closestBet}`;
 }
 
 //TEN
 
 function placeBetTenPayoff(bet) {
     let payoff = ((bet / 5) * 9);
+    document.getElementById('payoff-ten').textContent=`Bet! $${bet} pays $${payoff}`;
+
     console.log(`paying bet: $${bet} 10 amount: $${payoff}`);
     return payoff;
 }
@@ -131,27 +129,28 @@ function placeBetTenPayoff(bet) {
 function isTenBetValid(bet) {
     console.log(`isTenBetValid checked for proper bet: ${bet} on the 10`);
     if (bet >= 5, bet % 5 === 0) {
+        document.getElementById('ten-invalid').textContent='';
         return true;
     } else {
+        document.getElementById('place-ten').classList.add('is-invalid');
+        document.getElementById('ten-invalid').textContent= closestBetOutsideNums(bet);
         return false;
     }
 }
 
 
-function closestProperTenBet(bet) {
+function closestBetOutsideNums(bet) {
     let closestBet = Math.round(bet / 5)* 5;
-    return `${bet} is not proper for the 10. Nearest proper bet is ${closestBet}`;
+    return `${bet} is not a proper bet. Nearest proper bet is ${closestBet}`;
+}
+
+function closestSixEightBet(bet) {
+    let closestBet = Math.round(bet / 6)*6;
+    return `${bet} is not a proper bet. Nearest proper bet is ${closestBet}`;
 }
 
 
-//     function areOutsideNumbersValid(bet) {
-//         console.log(`areOutsideNumbersValid checked for proper bet: ${bet} on the numbers 4,5,9,10`);
-//         if (bet >= 5, bet % 5 === 0) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
+//VALIDATION
 
 function isFieldValid(inputId, inputErrorId) {
     let input = document.getElementById(inputId).value;
@@ -165,21 +164,54 @@ function isFieldValid(inputId, inputErrorId) {
      } else {
          document.getElementById(inputId).classList.remove('is-invalid');
        inputError.textContent = '';
-       console.log(`added class 'is-valid to input id:${inputId}`)
+       console.log(`removed class 'is-invalid to input id:${inputId}`)
      return true
      }
  }
+
+
+ function clearFields(){
+    document.getElementById('payoff-four').textContent='';
+    document.getElementById('payoff-five').textContent='';
+    document.getElementById('payoff-six').textContent='';
+    document.getElementById('payoff-eight').textContent='';
+    document.getElementById('payoff-nine').textContent='';
+    document.getElementById('payoff-ten').textContent='';
+
+    document.getElementById('four-invalid').textContent='';
+    document.getElementById('five-invalid').textContent='';
+    document.getElementById('six-invalid').textContent='';
+    document.getElementById('eight-invalid').textContent='';
+    document.getElementById('nine-invalid').textContent='';
+    document.getElementById('ten-invalid').textContent='';
+
+    document.getElementById('empty-field-four').textContent='';
+    document.getElementById('empty-field-five').textContent='';
+    document.getElementById('empty-field-six').textContent='';
+    document.getElementById('empty-field-eight').textContent='';
+    document.getElementById('empty-field-nine').textContent='';
+    document.getElementById('empty-field-ten').textContent='';
+    
+    document.getElementById('place-four').classList.remove('is-invalid');
+    document.getElementById('place-five').classList.remove('is-invalid');
+    document.getElementById('place-six').classList.remove('is-invalid');
+    document.getElementById('place-eight').classList.remove('is-invalid');
+    document.getElementById('place-nine').classList.remove('is-invalid');
+    document.getElementById('place-ten').classList.remove('is-invalid');
+ }
+
+
 //BUTTONS
 
 //FOUR
     document.getElementById('place-four-btn').addEventListener('click', ()=> {
-        console.log('button 4 is working');
         let bet = document.getElementById('place-four').value;
         let fieldValid = isFieldValid('place-four', 'empty-field-four');
         let fourValid = isFourBetValid(bet);
 
         if(fourValid && fieldValid){
-                placeBetFourPayoff(bet);
+            clearFields();
+            placeBetFourPayoff(bet);
             }
 
         document.getElementById('place-four').value='';
@@ -189,8 +221,15 @@ function isFieldValid(inputId, inputErrorId) {
 
 //FIVE
     document.getElementById('place-five-btn').addEventListener('click', ()=> {
-        console.log('button 5 is working');
+        let bet = document.getElementById('place-five').value;
+        let fieldValid = isFieldValid('place-five', 'empty-field-five');
+        let fiveValid = isFiveBetValid(bet);
 
+        if(fiveValid && fieldValid){
+            clearFields()
+            placeBetFivePayoff(bet)
+
+        }
 
         document.getElementById('place-five').value='';
         document.getElementById('place-five').focus();
@@ -200,11 +239,15 @@ function isFieldValid(inputId, inputErrorId) {
 
 //SIX
     document.getElementById('place-six-btn').addEventListener('click', ()=> {
-        console.log('button 6 is working');
         let bet = document.getElementById('place-six').value;
-        if(isSixBetValid(bet)){
+        let fieldValid = isFieldValid('place-six', 'empty-field-six');
+        let sixValid = isSixBetValid(bet);
+
+        if(sixValid && fieldValid){
+            clearFields()
             placeBetSixPayoff(bet);
         }
+
         document.getElementById('place-six').value='';
         document.getElementById('place-six').focus();
 
@@ -213,13 +256,15 @@ function isFieldValid(inputId, inputErrorId) {
 
 //EIGHT    
     document.getElementById('place-eight-btn').addEventListener('click', ()=> {
-        console.log('button 8 is working');
         let bet = document.getElementById('place-eight').value;
+        let fieldValid = isFieldValid('place-eight', 'empty-field-eight');
+        let eightValid = isEightBetValid(bet);
 
-        if(isEightBetValid(bet)){
+        if (eightValid && fieldValid) {
+            clearFields()
             placeBetEightPayoff(bet);
         }
-
+  
 
         document.getElementById('place-eight').value='';
         document.getElementById('place-eight').focus();
@@ -229,9 +274,14 @@ function isFieldValid(inputId, inputErrorId) {
 
 //NINE    
     document.getElementById('place-nine-btn').addEventListener('click', ()=> {
-        console.log('button 9 is working');
+        let bet = document.getElementById('place-nine').value;
+        let fieldValid = isFieldValid('place-nine', 'empty-field-nine');
+        let nineValid = isNineBetValid(bet);
 
-
+        if(nineValid && fieldValid) {
+            clearFields()
+            placeBetNinePayoff(bet);
+        }
 
         document.getElementById('place-nine').value='';
         document.getElementById('place-nine').focus();
@@ -241,7 +291,14 @@ function isFieldValid(inputId, inputErrorId) {
 
 //TEN    
     document.getElementById('place-ten-btn').addEventListener('click', ()=> {
-        console.log('button 10 is working');
+        let bet = document.getElementById('place-ten').value;
+        let fieldValid = isFieldValid('place-ten', 'empty-field-ten');
+        let tenValid = isTenBetValid(bet);
+
+        if (tenValid && fieldValid) {
+            clearFields()
+            placeBetTenPayoff(bet);
+        }
 
         document.getElementById('place-ten').value='';
         document.getElementById('place-ten').focus();
